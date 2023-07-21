4a92b3788dd7998dfe5c221f2f9d07a5d40e56e4
jp3pe <rlaworhkd430430@gmail.com>
2023-07-21 16:35:11 +0900

    Use data structure for URL to generate URL

--- quickstart.py
+++ quickstart.py
@@ -47,18 +47,26 @@ def main():
     new_event: GoogleCalendarEventApi = GoogleCalendarEventApi()
     # Get user input about Google Calendar event
     print("Let's create a link for new event in Google Calendar!")
-    new_event.summary = parse.quote(input("Name of the event: "))
-    new_event.location = parse.quote(input("Location of the event: "))
-    new_event.description = parse.quote(input("Description of the event: "))
-    date_from = input("Date from(ex: 2023-01-01T10:00:00): ")
-    date_until = input("Date until(ex: 2023-01.02T11:00:00): ")
-    new_event.start_date_time = datetime_string_to_google_calendar_url_date_format_converter(date_from)
-    new_event.end_date_time = datetime_string_to_google_calendar_url_date_format_converter(date_until)
+    new_event.summary = input("Name of the event: ")
+    new_event.location = input("Location of the event: ")
+    new_event.description = input("Description of the event: ")
+    new_event.start_date_time = input("Date from(ex: 2023-01-01T10:00:00): ")
+    new_event.end_date_time = input("Date until(ex: 2023-01.02T11:00:00): ")
 
-    url: str = f"https://calendar.google.com/calendar/r/eventedit?action=TEMPLATE&dates={new_event.start_date_time}" \
-               f"%2F{new_event.end_date_time}&stz={new_event.start_time_zone}&etz={new_event.end_time_zone}&details" \
-               f"={new_event.description}&location" \
-               f"={new_event.location}&text={new_event.summary}"
+    new_event_url = GoogleCalendarEventUrl(text=parse.quote(new_event.summary),
+                                           date_from=datetime_string_to_google_calendar_url_date_format_converter(
+                                               new_event.start_date_time),
+                                           date_until=datetime_string_to_google_calendar_url_date_format_converter(
+                                               new_event.end_date_time),
+                                           stz=new_event.start_time_zone,
+                                           etz=new_event.end_time_zone,
+                                           details=parse.quote(new_event.description),
+                                           location=parse.quote(new_event.location)
+                                           )
+
+    url: str = f"https://calendar.google.com/calendar/r/eventedit?action=TEMPLATE&dates={new_event_url.date_from}" \
+               f"%2F{new_event_url.date_until}&stz={new_event_url.stz}&etz={new_event_url.etz}&details" \
+               f"={new_event_url.details}&location={new_event_url.location}&text={new_event_url.text}"
     description_message: str = f'Please click the link to add an event into your Google Calendar: {url}'
     print(description_message)
 
